d389247be95639c9efbc1c393d7410b0ca694191
Kangcheng Hou <kangchenghou@gmail.com>
2022-01-13 12:39:51 -0800

    cleaner notebook

--- admix_prs/_utils.py
+++ admix_prs/_utils.py
@@ -3,15 +3,16 @@ import pandas as pd
 from tqdm import tqdm
 from os.path import join
 
+
 def plink2_assoc_to_ldpred2(plink2_assoc_path):
     assoc = pd.read_csv(plink2_assoc_path, delim_whitespace=True)
     assert np.all(assoc["A1"] == assoc["ALT"])
-#     flip_pos = assoc["A1"] != assoc["ALT"]
-#     print(
-#         f"{np.mean(flip_pos):.2g} of SNPs are flipped,"
-#         " fliping BETA and T_STAT at these location"
-#     )
-#     assoc.loc[flip_pos, ["BETA", "T_STAT"]] *= -1
+    #     flip_pos = assoc["A1"] != assoc["ALT"]
+    #     print(
+    #         f"{np.mean(flip_pos):.2g} of SNPs are flipped,"
+    #         " fliping BETA and T_STAT at these location"
+    #     )
+    #     assoc.loc[flip_pos, ["BETA", "T_STAT"]] *= -1
     assoc = assoc[
         [
             "#CHROM",
