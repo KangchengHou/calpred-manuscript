bae6ccaa76626f58fd5e07411d943b8f8d750cf7
Kangcheng Hou <kangchenghou@gmail.com>
2022-01-14 10:00:49 -0800

    add evaluate simulation notebook

--- admix_prs/_utils.py
+++ admix_prs/_utils.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from os.path import join
+from tqdm import tqdm
+from typing import List
 
 
 def plink2_assoc_to_ldpred2(plink2_assoc_path):
@@ -38,3 +40,56 @@ def plink2_assoc_to_ldpred2(plink2_assoc_path):
         }
     )
     return assoc
+
+
+def load_sim_data(
+    config: str,
+    DATA_DIR: str = "/u/project/sgss/UKBB/PRS-RESEARCH/02-yi-simulate-prs/subcontinenal/",
+    n_sim: int = 5,
+) -> List[pd.DataFrame]:
+    """
+    Parameters
+    ----------
+    config: str
+        simulation parameter configuration
+
+    DATA_DIR: str
+        root directory to read the data
+
+    n_sim: int
+        number of simulations to read
+
+    Returns
+    -------
+    A list of `n_sim` pd.DataFrame, with each data frame containing
+        - PRS_MEAN: PRS point estimate
+        - PRS_STD: PRS uncertainty standard deviation
+        - PHENO: phenotype
+        - PHENO_G: genetic component of phenotype
+    """
+    df_pheno_g = pd.read_csv(
+        join(DATA_DIR, config, "data/sim.pheno_g.tsv.gz"), sep="\t"
+    )
+    df_pheno = pd.read_csv(join(DATA_DIR, config, "data/sim.pheno.tsv.gz"), sep="\t")
+
+    for df in [df_pheno_g, df_pheno]:
+        df.index = df.FID.astype(str) + "_" + df.IID.astype(str)
+        df.drop(columns=["FID", "IID"], inplace=True)
+
+    df_res_list = []
+    for i in tqdm(range(n_sim)):
+        df_prs = pd.read_csv(
+            join(DATA_DIR, config, f"predict/SIM_{i}.auto.test.test_prs.tsv.gz"),
+            sep="\t",
+            index_col=0,
+        ).drop(columns=["MEAN"])
+        # center with column mean
+        df_prs -= df_prs.mean(axis=0)
+        df_res = pd.DataFrame(
+            {"PRS_MEAN": df_prs.mean(axis=1), "PRS_STD": df_prs.std(axis=1)}
+        )
+        df_res["PHENO"] = df_pheno[f"SIM_{i}"].reindex(df_res.index)
+        df_res["PHENO_G"] = df_pheno_g[f"SIM_{i}"].reindex(df_res.index)
+        df_res_list.append(df_res)
+
+    return df_res_list
---
+++ notebooks/README.md
@@ -0,0 +1,4 @@
+# Notebooks
+
+1. `evaluate-real.ipynb`: Example of evaluation that can be performed on real data.
+2. `evaluate-sim.ipynb`: Example of evaluation that can be performed on simulation data.
