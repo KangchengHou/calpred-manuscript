c53c39a4bbcb0fce0faa00a22ba8332bb9e1d0d2
Kangcheng Hou <kangchenghou@gmail.com>
2022-02-28 10:15:32 -0800

    add example for testing R2 diff

--- README.md
+++ README.md
@@ -10,6 +10,13 @@ To use the package (for example),
 import admix_prs
 ?admix_prs.calibrate_prs
 ```
+
+We take input of PRS point estimate (mean), as well as the \alpha-level credible interval (lower-ci, upper-ci), in addition to a set of individuals used to perform the calibration. We aim to produce a tight and calibrated prediction interval.
+
+The pipeline goes as follows:
+1. Obtaining the PRS prediction (mean + std) of genetic risk for every individual in the data set (since PRS is usually consisting of hundreds of SNPs, the error terms are likely well-approximated by a Gaussian, which can be described with only mean + std)
+2. Since step 1 only contains only genetic risk (the std only capture the genetic risk uncertainty). We perform two steps:
+    (a)
 ## File structures
 
 - `admix_prs/`: source code
--- admix_prs/_evaluate.py
+++ admix_prs/_evaluate.py
@@ -1,3 +1,5 @@
+from pyexpat.errors import XML_ERROR_SYNTAX
+from matplotlib.axis import XAxis
 import numpy as np
 import pandas as pd
 import random
@@ -5,6 +7,7 @@ from scipy import stats
 from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
 import uncertainty_toolbox as uct
+from tqdm import tqdm
 
 
 def uct_metrics(pred_mean, pred_std, y):
@@ -14,16 +17,19 @@ def uct_metrics(pred_mean, pred_std, y):
     return metrics
 
 
-def evaluate(
+def summarize_pred(
     df: pd.DataFrame,
     y_col: str,
     pred_col: str,
-    predstd_col: str,
+    predstd_col: str = None,
     ci=0.90,
     group_col: str = None,
+    n_bootstrap: int = 0,
+    return_r2_diff=False,
 ):
     """
-    Evaluate R2, coverage, interval length for different groups.
+    Summarize the results of prediction, produce R2, coverage, interval length for
+        different groups.
 
     Parameters
     ----------
@@ -34,10 +40,12 @@ def evaluate(
         Name of the column containing the true value.
     pred_col : str
         predicted value
-    std_col : str
+    predstd_col : str
         standard deviation of the prediction
     group_col : str
         Name of the column containing the group variable.
+    return_se : bool
+        whether to return standard error of prediction,
 
     Returns
     -------
@@ -52,43 +60,75 @@ def evaluate(
             lambda df: stats.linregress(df[pred_col], df[y_col]).rvalue ** 2
         )
 
-        coverage = df_grouped.apply(
-            lambda df: df[y_col]
-            .between(
-                df[pred_col] - df[predstd_col] * ci_z,
-                df[pred_col] + df[predstd_col] * ci_z,
-            )
-            .mean()
-        )
-        length = df_grouped.apply(lambda df: (df[predstd_col] * ci_z).mean())
         y_std = df_grouped.apply(lambda df: df[y_col].std())
         pred_std = df_grouped.apply(lambda df: df[pred_col].std())
-        df = {
+        df_res = {
             "r2": r2,
-            "coverage": coverage,
-            "length": length,
-            "y_std": y_std,
-            "pred_std": pred_std,
+            "std(y)": y_std,
+            "std(pred)": pred_std,
         }
-        return pd.DataFrame(data=df)
+
+        if predstd_col is not None:
+            df_res["coverage"] = df_grouped.apply(
+                lambda df: df[y_col]
+                .between(
+                    df[pred_col] - df[predstd_col] * ci_z,
+                    df[pred_col] + df[predstd_col] * ci_z,
+                )
+                .mean()
+            )
+            df_res["length"] = df_grouped.apply(
+                lambda df: (df[predstd_col] * ci_z).mean()
+            )
+        df_res = pd.DataFrame(df_res)
     else:
         r2 = stats.linregress((df[pred_col]) / 2, df[y_col]).rvalue ** 2
-        coverage = (
-            df[y_col]
-            .between(
-                df[pred_col] - df[predstd_col] * ci_z,
-                df[pred_col] + df[predstd_col] * ci_z,
-            )
-            .mean()
-        )
-        length = (df[predstd_col] * ci_z).mean()
         y_std = df[y_col].std()
         pred_std = df[pred_col].std()
-        df = {
+
+        df_res = {
             "r2": r2,
-            "coverage": coverage,
-            "length": length,
-            "y_std": y_std,
-            "pred_std": pred_std,
+            "std(y)": y_std,
+            "std(pred)": pred_std,
         }
-        return pd.Series(data=df)
+        if predstd_col is not None:
+
+            df_res["coverage"] = (
+                df[y_col]
+                .between(
+                    df[pred_col] - df[predstd_col] * ci_z,
+                    df[pred_col] + df[predstd_col] * ci_z,
+                )
+                .mean()
+            )
+            df_res["length"] = (df[predstd_col] * ci_z).mean()
+        df_res = pd.Series(df_res)
+    if n_bootstrap == 0:
+        return df_res
+    else:
+        bootstrap_dfs = []
+        for _ in tqdm(range(n_bootstrap), desc="Bootstrapping"):
+            # sample with replacement
+            bootstrap_dfs.append(
+                summarize_pred(
+                    df.sample(frac=1, replace=True),
+                    y_col,
+                    pred_col,
+                    predstd_col,
+                    ci,
+                    group_col,
+                    n_bootstrap=0,
+                )
+            )
+        df_res_se = pd.DataFrame(
+            np.dstack(bootstrap_dfs).std(axis=2),
+            index=df_res.index,
+            columns=df_res.columns,
+        )
+        if (group_col is not None) and return_r2_diff:
+            r2_diff = np.array(
+                [d["r2"].iloc[-1] - d["r2"].iloc[0] for d in bootstrap_dfs]
+            )
+            return df_res, df_res_se, r2_diff
+        else:
+            return df_res, df_res_se
