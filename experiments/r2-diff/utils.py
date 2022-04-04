8fd13cfc1309ac1197cfe5e626799baf2cd4d7e0
Kangcheng Hou <kangchenghou@gmail.com>
2022-04-04 14:20:29 -0700

    add r2 difference experiment

---
+++ experiments/r2-diff/utils.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import os
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import glob
+import statsmodels.api as sm
+from scipy.stats import pearsonr
