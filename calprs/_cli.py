747251c4fdd3b4dccb7cc9480db3642f37215172
Ziqi Xu <ziqixu09@n6448.hoffman2.idre.ucla.edu>
2022-04-18 11:56:52 -0700

    cli update

--- calprs/_cli.py
+++ calprs/_cli.py
@@ -32,9 +32,28 @@ def r2diff(df: str, y: str, pred: str, group: Union[str, List[str]], out: str):
     out : str
         Path to the output file.
     """
-    log_params("r2diff", locals())
     df = pd.read_csv(df, sep='\t', index_col=0)
-    print(df)
+    
+    out_li = []
+    n_unique = len(np.unique(df[group].values))
+    if n_unique > 5:
+        df[group] = pd.qcut(df[group], q=5, duplicates="drop").cat.codes
+        print(f"Converting column '{group}' to 5 quintiles")
+    df_res, df_res_se, r2_diff = admix_prs.summarize_pred(
+        df,
+        y_col=y,
+        pred_col=pred,
+        group_col=group,
+        n_bootstrap=n_bootstrap,
+        return_r2_diff=True,
+    )
+    out_li.append(
+        [group, df_res["r2"].iloc[-1] - df_res["r2"].iloc[0], np.mean(r2_diff > 0)]
+    )
+    
+    df_out = pd.DataFrame(out_li, columns = ["group", "r2diff", "p_value"])
+    os.makedirs(out, exist_ok=True) 
+    df_out.to_csv(str(out) + "r2diff_" + str(y) + '_'+ str(group) + ".csv")
 
 
 def model(
