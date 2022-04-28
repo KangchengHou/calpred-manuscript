c65eb15e93ee632a9eaab76326cc9e9076891113
Ziqi Xu <ziqixu091@github.com>
2022-04-28 11:21:04 -0700

    update test for model and calibrate cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -26,3 +26,35 @@ def test_r2diff():
     # test difference
     assert np.allclose(df_out["r2diff"].values, [-0.0740155, -0.0740089])
     assert np.allclose(df_out["prob>0"].values, [0.018, 0.0])
+
+
+def test_model_calibrate():
+    toy_data = os.path.join(calprs.get_data_folder(), "toy.tsv")
+
+    tmp_dir = tempfile.TemporaryDirectory()
+    out_path_1 = tmp_dir.name + "model_out.tsv"
+    cmds_1 = [
+        "calprs model",
+        f"--df {toy_data}",
+        "--y y",
+        "--pred prs",
+        "--predstd predstd_base" "--ci_method scale",
+        f"--out {out_path_1}",
+    ]
+    subprocess.check_call(" ".join(cmds_1), shell=True)
+
+    out_path_2 = tmp_dir.name + "cali_out.tsv"
+    cmds_2 = [
+        "calprs calibrate",
+        f"model {out_path_1}" f"--df {toy_data}",
+        "--pred prs",
+        "--predstd predstd_base",
+        f"--out {out_path_2}",
+    ]
+    subprocess.check_call(" ".join(cmds_2), shell=True)
+    df_out = pd.read_csv(out_path_2, index_col=0, sep="\t")
+    tmp_dir.cleanup()
+
+    # test difference
+    assert np.allclose(df_out["calprs"].values, [-1.8686196, 2.0814843])
+    assert np.allclose(df_out["calpredstd"].values, [0.7683203, 0.8984368])
