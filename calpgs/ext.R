54508c7064f1094044aaa75c4ad384499f0ab240
Kangcheng Hou <kangchenghou@gmail.com>
2022-07-03 13:21:27 -0700

    refactor calibrate adjustment

---
+++ calpgs/ext.R
@@ -0,0 +1,64 @@
+library(statmod)
+fit_het_linear <- function(y,
+                           mean_covar,
+                           var_covar,
+                           slope_covar,
+                           tol = 1e-6, maxit = 500) {
+    # fit y ~ N((mean_covar * mean_beta) (1 + slope_covar * slope_beta),
+    # exp(var_covar * var_beta))))
+    if (missing(slope_covar)) {
+        # default back to remlscore
+        fit <- remlscore(
+            y = y,
+            X = mean_covar,
+            Z = var_covar,
+            tol = tol, maxit = maxit
+        )
+        return(list(
+            mean_beta = fit$beta,
+            var_beta = fit$gamma,
+            mean_beta_varcov = fit$cov.beta,
+            var_beta_varcov = fit$cov.gam
+        ))
+    } else {
+        # coordinate descent to optimize slope_beta
+        slope_coef <- rep(0, ncol(slope_covar))
+        for (i in 1:maxit) {
+            slope <- 1 + slope_covar %*% slope_coef
+
+            fit <- remlscore(
+                y = y,
+                X = sweep(mean_covar, MARGIN = 1, slope, "*"),
+                Z = var_covar,
+                tol = tol, maxit = maxit
+            )
+            fitted_mu <- mean_covar %*% fit$beta
+            fitted_var <- as.vector(exp(var_covar %*% fit$gamma))
+
+            slope_coef_updated <- lm.wfit(
+                x = sweep(slope_covar, MARGIN = 1, fitted_mu, "*"),
+                y = y - fitted_mu,
+                w = 1 / fitted_var
+            )$coef
+
+            if (max(abs(slope_coef_updated - slope_coef)) < tol) {
+                break
+            }
+            slope_coef <- slope_coef_updated
+            if (i == maxit) {
+                print(paste0(
+                    "Maximum number of iterations reached.",
+                    " May not have converged. Consider increase maxit."
+                ))
+            }
+        }
+
+        return(list(
+            mean_beta = fit$beta,
+            var_beta = fit$gamma,
+            mean_beta_varcov = fit$cov.beta,
+            var_beta_varcov = fit$cov.gam,
+            slope_beta = slope_coef_updated
+        ))
+    }
+}
