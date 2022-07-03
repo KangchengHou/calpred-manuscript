e84b9f226609140d04d0fc5a1db09a16db12320c
Kangcheng Hou <kangchenghou@gmail.com>
2022-07-03 14:27:36 -0700

    update fit_het_linear

--- calpgs/ext.R
+++ calpgs/ext.R
@@ -1,9 +1,11 @@
 library(statmod)
+
 fit_het_linear <- function(y,
                            mean_covar,
                            var_covar,
                            slope_covar,
-                           tol = 1e-6, maxit = 500) {
+                           tol = 1e-6,
+                           maxit = 500) {
     # fit y ~ N((mean_covar * mean_beta) (1 + slope_covar * slope_beta),
     # exp(var_covar * var_beta))))
     if (missing(slope_covar)) {
@@ -35,16 +37,16 @@ fit_het_linear <- function(y,
             fitted_mu <- mean_covar %*% fit$beta
             fitted_var <- as.vector(exp(var_covar %*% fit$gamma))
 
-            slope_coef_updated <- lm.wfit(
+            slope_fit <- lm.wfit(
                 x = sweep(slope_covar, MARGIN = 1, fitted_mu, "*"),
                 y = y - fitted_mu,
                 w = 1 / fitted_var
-            )$coef
-
-            if (max(abs(slope_coef_updated - slope_coef)) < tol) {
+            )
+            # compare with previous iteration
+            if (max(abs(slope_fit$coef - slope_coef)) < tol) {
                 break
             }
-            slope_coef <- slope_coef_updated
+            slope_coef <- slope_fit$coef
             if (i == maxit) {
                 print(paste0(
                     "Maximum number of iterations reached.",
@@ -52,13 +54,23 @@ fit_het_linear <- function(y,
                 ))
             }
         }
+        # lm.wfit does not provide vcov matrix, redo the fit using `lm`
+        slope_fit <- lm(
+            formula =
+                (y - fitted_mu) ~
+                sweep(slope_covar, MARGIN = 1, fitted_mu, "*") - 1,
+            weights = 1 / fitted_var
+        )
 
+        # Reference for slope_fit vcov
+        # https://stats.stackexchange.com/questions/113987/lm-weights-and-the-standard-error
         return(list(
             mean_beta = fit$beta,
             var_beta = fit$gamma,
+            slope_beta = slope_fit$coef,
             mean_beta_varcov = fit$cov.beta,
             var_beta_varcov = fit$cov.gam,
-            slope_beta = slope_coef_updated
+            slope_beta_varcov = vcov(slope_fit) / summary(slope_fit)$sigma^2
         ))
     }
 }
