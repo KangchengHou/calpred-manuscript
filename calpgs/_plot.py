d40d8d2d4099d10006633110d4d86b52de81f1a5
Kangcheng Hou <kangchenghou@gmail.com>
2022-08-15 16:05:43 -0700

    update plotting code

--- calpgs/_plot.py
+++ calpgs/_plot.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import statsmodels.api as sm
 import seaborn as sns
+import matplotlib.transforms as mtrans
+from typing import Dict
 
 
 def plot_calibration(
@@ -102,3 +103,127 @@ def compare_values(
         ax.set_xlabel(xlabel)
     if ylabel is not None:
         ax.set_ylabel(ylabel)
+
+
+def plot_heatmap(
+    df_val: pd.DataFrame,
+    df_annot: pd.DataFrame = None,
+    annot_kws: Dict = None,
+    cmap="RdBu_r",
+    dpi=150,
+    squaresize=20,
+    heatmap_linewidths=0.5,
+    heatmap_linecolor="gray",
+    heatmap_xticklabels=True,
+    heatmap_yticklabels=True,
+    heatmap_cbar=True,
+    heatmap_cbar_kws=dict(use_gridspec=False, location="top", fraction=0.03, pad=0.01),
+    heatmap_vmin=-5,
+    heatmap_vmax=5,
+    xticklabels_rotation=45,
+):
+    """
+    Plot heatmap with annotations.
+    df_val: pd.DataFrame
+        The dataframe with the values to plot.
+    df_annot: pd.DataFrame
+        The dataframe with the annotations to plot.
+    """
+    figwidth = df_val.shape[1] * squaresize / float(dpi)
+    figheight = df_val.shape[0] * squaresize / float(dpi)
+    fig, ax = plt.subplots(1, figsize=(figwidth, figheight), dpi=dpi)
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+
+    sns.heatmap(
+        df_val,
+        cmap=cmap,
+        linewidths=heatmap_linewidths,
+        linecolor=heatmap_linecolor,
+        square=True,
+        annot=df_annot,
+        annot_kws=annot_kws,
+        fmt="",
+        ax=ax,
+        xticklabels=heatmap_xticklabels,
+        yticklabels=heatmap_yticklabels,
+        cbar=heatmap_cbar,
+        cbar_kws=heatmap_cbar_kws,
+        vmin=heatmap_vmin,
+        vmax=heatmap_vmax,
+    )
+
+    plt.yticks(fontsize=8)
+    ax.set_xticklabels(
+        ax.get_xticklabels(),
+        rotation=xticklabels_rotation,
+        va="top",
+        ha="right",
+        fontsize=8,
+    )
+    ax.tick_params(left=False, bottom=False, pad=-2)
+    trans = mtrans.Affine2D().translate(5, 0)
+    for t in ax.get_xticklabels():
+        t.set_transform(t.get_transform() + trans)
+    return fig, ax
+
+
+def plot_r2_heatmap(
+    df_val: pd.DataFrame, df_annot: pd.DataFrame, cbar_pad=0.04, cbar_fraction=0.0188
+):
+    """Plot heatmap of variable R2
+
+    Parameters
+    ----------
+    df_val : pd.DataFrame
+        R2 differences.
+    df_annot : pd.DataFrame
+        annotations.
+    cbar_pad : float, optional
+        pad for colorbar, by default 0.04
+    cbar_fraction : float, optional
+        fraction for colorbar, by default 0.0188
+
+    Returns
+    -------
+    fig, ax
+    """
+    fig, ax = plot_heatmap(
+        df_val=df_val,
+        df_annot=df_annot,
+        annot_kws={"fontsize": 6, "weight": "bold"},
+        cmap=plt.get_cmap("bwr", 11),
+        squaresize=45,
+        heatmap_vmin=-0.5,
+        heatmap_vmax=0.5,
+        heatmap_linecolor="white",
+        heatmap_linewidths=1.0,
+        heatmap_cbar_kws=dict(
+            use_gridspec=False,
+            location="right",
+            fraction=cbar_fraction,
+            pad=cbar_pad,
+            drawedges=True,
+        ),
+    )
+    ax.set_xlabel(None)
+    ax.set_xticklabels(ax.get_xticklabels(), fontsize=9)
+
+    ax.set_ylabel(None)
+    ax.set_yticklabels(ax.get_yticklabels(), fontsize=9)
+
+    cbar = ax.collections[0].colorbar
+    cbar.set_ticks([-0.5, -0.25, 0, 0.25, 0.5])
+    cbar.set_ticklabels(["<-50%", "-25%", "0%", "25%", ">50%"])
+    cbar.ax.set_ylabel(
+        "Relative $\Delta (R^2)$", rotation=270, fontsize=9, labelpad=6.0
+    )
+    cbar.outline.set_edgecolor("black")
+    cbar.outline.set_linewidth(0.8)
+
+    cbar.ax.tick_params(labelsize=8)
+    cbar.ax.tick_params(size=0)
+
+    for _, spine in ax.spines.items():
+        spine.set_visible(True)
+
+    return fig, ax
